945a2f8cc9bdca69746f2d4a1c3efb4b7f59043c
Shawn Rakowski <srakowski@live.com>
2015-12-11 23:50:54 -0600

    intial screen and hook into Coldsteel (github.com/srakowski/coldsteel)

---
+++ LD34/Game1.cs
@@ -0,0 +1,43 @@
+﻿using Coldsteel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace LD34
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        ScreenManager _screenManager;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+
+            _screenManager = new ScreenManager(this);
+            _screenManager.BackgroundColor = Color.CornflowerBlue;
+            this.Components.Add(_screenManager);
+
+
+            Content.RootDirectory = "Content";
+        }
+
+#if WINDOWS || LINUX
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+#endif
+    }
+}
